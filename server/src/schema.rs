eae81055093da980a7424472d5b813fb2ae5c30d
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2023-09-23 10:28:15 -0700

    Dynamic plugin endpoints PoC.

--- server/src/schema.rs
+++ server/src/schema.rs
@@ -7,11 +7,23 @@ use async_graphql::dynamic::TypeRef;
 use async_graphql::Value;
 
 pub fn schema() -> Result<Schema, SchemaError> {
-    let query = Object::new("Qurey").field(Field::new(
+    let mut query = Object::new("Qurey").field(Field::new(
         "hostname",
         TypeRef::named(TypeRef::STRING),
         |_| FieldFuture::new(async { Ok(Some(Value::from("example.com"))) }),
     ));
+    let plugins: Vec<&str> = vec!["foo", "bar"];
+    let mut fields: Vec<Field> = vec![];
+    for plugin in plugins {
+        fields.push(Field::new(
+            plugin,
+            TypeRef::named(TypeRef::STRING),
+            move |_| FieldFuture::new(async move { Ok(Some(Value::from(plugin))) }),
+        ));
+    }
+    for f in fields {
+        query = query.field(f);
+    }
     Schema::build(query.type_name(), None, None)
         .register(query)
         .finish()
