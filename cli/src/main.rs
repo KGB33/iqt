c9e0c2f5d8bacb6467fed18489da5d723c070c61
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2023-10-02 16:30:00 -0700

    feat(cli): Inital CLI flags.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// The graphql query to run
+    query: String,
+
+    /// Specify a subnet in CIDR notation (e.g., 10.0.9.0/24)
+    #[clap(short, long)]
+    subnets: Option<Vec<String>>,
+
+    /// Specify one or more host IP addresses
+    #[clap(short, long)]
+    inventory: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!(
+        "subnets: {:?} - inventory_file: {:?} - query: {:?}",
+        cli.subnets.as_deref(),
+        cli.inventory.as_deref(),
+        cli.query
+    )
 }
